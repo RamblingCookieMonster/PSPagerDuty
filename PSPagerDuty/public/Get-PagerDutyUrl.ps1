c71bfea60779b8d9be92f9bb70267af2bd7f566d
Romain Tiennot <romain.tiennot@manomano.com>
2021-11-01 18:47:04 +0100

    create new function with all endpoint url

---
+++ PSPagerDuty/public/Get-PagerDutyUrl.ps1
@@ -0,0 +1,46 @@
+﻿function Get-PagerDutyUrl
+{
+    <#
+    .SYNOPSIS
+       Get PSPagerDuty urls and content type
+
+    .DESCRIPTION
+       Get PSPagerDuty urls
+
+    .EXAMPLE
+        Get-PagerDutyUrl
+
+    .FUNCTIONALITY
+        PagerDuty
+    #>
+
+    [CmdletBinding()]
+    [OutputType('System.Collections.Hashtable')]
+    param(
+    )
+
+    begin
+    {
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
+        $HashUrl = @{
+            Api    = @{
+                url         = 'https://api.pagerduty.com'
+                contenttype = 'application/json'
+            }
+            Events = @{
+                url         = 'https://events.pagerduty.com/v2/enqueue'
+                contenttype = 'application/json'
+            }
+        }
+    }
+
+    process
+    {
+    }
+
+    end
+    {
+        return $HashUrl
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function ended"
+    }
+}
